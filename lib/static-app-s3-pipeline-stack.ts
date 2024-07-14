d971c552ed78f8c399099bfdc21d7dea2c864c4e
Jon Richards <jonr.web@gmail.com>
2024-07-14 12:09:54 +0100

    add precommit stuff

--- lib/static-app-s3-pipeline-stack.ts
+++ lib/static-app-s3-pipeline-stack.ts
@@ -1,8 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class StaticAppS3PipelineStack extends cdk.Stack {
+  /* eslint @typescript-eslint/no-useless-constructor: "off" */
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
