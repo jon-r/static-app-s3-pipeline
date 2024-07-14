71ed35c2fe940eb8b6e214cf549959770a3cf733
Jon Richards <jonr.web@gmail.com>
2024-07-14 11:37:19 +0100

    Initial commit

---
+++ lib/static-app-s3-pipeline-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class StaticAppS3PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'StaticAppS3PipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
