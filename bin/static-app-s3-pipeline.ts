0fd79893022d39bb064deecd177743b502bb9e83
Jon Richards <jonr.web@gmail.com>
2024-07-15 15:17:01 +0100

    update config

--- bin/static-app-s3-pipeline.ts
+++ bin/static-app-s3-pipeline.ts
@@ -13,6 +13,6 @@ new StaticAppS3PipelineStack(app, "StaticAppS3PipelineStack", {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: import.meta.env.VITE_ACCOUNT_ID, region: "eu-west-2" },
+  env: { account: import.meta.env.VITE_ACCOUNT_ID, region: "us-east-1" },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
